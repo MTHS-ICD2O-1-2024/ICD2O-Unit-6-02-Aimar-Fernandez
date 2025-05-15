d80cbe218732f67a75c48125d0591f6dd3e951af
Aimar Fernandez <aimar.fernandez1@stu.ocsb.ca>
2025-05-15 17:28:47 +0000

    added js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,27 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Aimar Fernandez
+// Created on: May 2025
 // This file contains the JS functions for index.html
+
+"use strict"
+
+
+
+function updateCookieCount() {
+  if (sessionStorage.savedCookieNumber) {
+    
+  }
+  else {
+    sessionStorage.savedCookieNumber = 0
+  }
+  document.getElementById("answer").innerHTML = 
+    "<p>Total amount of cookies: " + sessionStorage.savedCookieNumber + "</p>"
+}
+
+
+function myButtonClicked() {
+  sessionStorage.savedCookieNumber = parseInt(sessionStorage.savedCookieNumber) + 1
+  document.getElementById("answer").innerHTML = 
+    "<p>Total amount of cookies: " + sessionStorage.savedCookieNumber + "</p>"
+}
